59cf7c4956606be6ef7f55f0fad0150e8863c4d0
kukaur <kukaur@blackberry.com>
2021-03-28 22:38:27 -0400

    area and commands added

---
+++ area.py
@@ -0,0 +1,8 @@
+#module is python scripts with reusable funtions
+
+import math
+
+r = float(input('please enter the radius'))
+#pi = 22/7
+area = math.pi*r**2
+print(area)
---
+++ command.py
@@ -0,0 +1,7 @@
+#filelocation,database,remoteip
+#argv List of arguments contained in sys modeule
+#how to pass command line arguments and  read
+import sys
+lst = sys.argv
+
+print('product is:',int(lst[1])*int(lst[2]))
